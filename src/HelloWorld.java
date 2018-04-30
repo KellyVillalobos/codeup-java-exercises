58ebe949a89ab04063057783077fe5871b2a67af
Kelly Villalobos <kvdesign1978@gmail.com>
2018-04-30 11:48:37 -0500

    first 2 on sntax and data types

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,14 @@
 public class HelloWorld{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 39;
+        System.out.println(myFavoriteNumber);
+
+        String myString ="My favorite number is: " + myFavoriteNumber;
+        System.out.println(myString);
+
+
     }
+
 }
