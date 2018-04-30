7338ac6d31c627ad094218529fb9d3f59be10be2
Kelly Villalobos <kvdesign1978@gmail.com>
2018-04-30 16:08:01 -0500

    input for width and length, make input integer with parseInt

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -25,15 +25,39 @@ public class ConsoleExercises {
 
 
 
-
-         System.out.print("Enter a sentence: ");
-
-         String sentenceString = scan.nextLine();
-
-        System.out.println("You typed: \"" + sentenceString );
+//
+//         System.out.print("Enter a sentence: ");
+//
+//         String sentenceString = scan.nextLine();
+//
+//        System.out.println("You typed: \"" + sentenceString );
 
 
 //        System.out.println("You Entered: --> \"" + userInput + "\" <--");
+
+
+
+        System.out.println("Enter length: ");
+
+        String x = scan.next();
+
+        int length = Integer.parseInt(x);
+
+
+        System.out.println("Enter width: ");
+
+        String y = scan.next();
+        int width =  Integer.parseInt(y);
+        int area;
+        int perimeter;
+        area = length * width;
+        perimeter = 2 * length + 2 * width;
+        System.out.println("The Width is: \"" + width);
+        System.out.println("The Length is \"" + length);
+        System.out.println("The area is: \"" + area);
+        System.out.println("the Perimeter of the room is: \"" + perimeter);
+
+
 
 
 
