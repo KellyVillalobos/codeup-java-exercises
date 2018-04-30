caf025f56cc9df35c6dde6911a415f6ab082ff91
Kelly Villalobos <kvdesign1978@gmail.com>
2018-04-30 17:01:07 -0500

    do while first example done

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,11 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while(i <= 15){
-            System.out.println(i);
-            ++i;
+       int i = -2;
 
-        }
+        do{
+             i = i + 2;
+            System.out.println(i);
+
+        }while(i < 100);
     }
 }
