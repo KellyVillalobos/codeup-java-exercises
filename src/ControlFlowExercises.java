6d5c447cb706e8a12ff7c7bddff28af3c8cb36a9
Kelly Villalobos <kvdesign1978@gmail.com>
2018-05-01 08:33:26 -0500

    refactor to count backwards from 100 to -10 by 5s

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,11 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-       int i = -2;
+       int i = 105;
 
         do{
-             i = i + 2;
+             i = i - 5;
             System.out.println(i);
 
-        }while(i < 100);
+        }while(i > -10);
     }
 }
