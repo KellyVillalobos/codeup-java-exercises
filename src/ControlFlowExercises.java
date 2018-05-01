82388b8b7f74a5d3e567e778c91e33050c1bb939
Kelly Villalobos <kvdesign1978@gmail.com>
2018-05-01 09:44:22 -0500

    forgot to commit 2's to 100

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,15 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
+        int i = 2;
 
-        for (int i = 5; i <= 15; i += 1) {
+        do{
             System.out.println(i);
-        }
+            i = i + 2;
+
+        }while
+
+                (i <= 100);
+
 
 
         }
