73a781e90b9621341eb9af184b1c54b9a22a45f1
Kelly Villalobos <kvdesign1978@gmail.com>
2018-05-01 09:53:19 -0500

    refactor do while squared to for loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,7 +2,7 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
 
 
-        for(int i = 100; i >= -10; i -= 5){
+        for(long i = 2L; i < Math.pow(10,6); i = i * i){
             System.out.println(i);
 
 
