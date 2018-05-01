3e3ce7a490cd25771e9e9aeb8f5e70a6a2a0bb42
Kelly Villalobos <kvdesign1978@gmail.com>
2018-05-01 09:49:15 -0500

    count backwards by 5's to -10

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,14 +1,12 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 2;
 
-        do{
+
+        for(int i = 100; i >= -10; i -= 5){
             System.out.println(i);
-            i = i + 2;
 
-        }while
 
-                (i <= 100);
+        }
 
 
 
