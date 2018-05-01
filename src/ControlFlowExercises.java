2005ab70fe765e8bf5cb876803c51a28738b9492
Kelly Villalobos <kvdesign1978@gmail.com>
2018-05-01 12:45:49 -0500

    table of Powers

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,19 +1,24 @@
+import java.util.Scanner;
+
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
+        String userInput;
+        Scanner scan;
+
+        scan = new Scanner(System.in);
+        System.out.println("What number would you like to go to?: ");
+        userInput = scan.nextLine();
+
+        System.out.println("Here's your table");
+        System.out.println("Number | Squared | Cubed ");
+        System.out.println("------ | ------- | ----- ");
+        for(int i = 1; i <= Integer.parseInt(userInput); i ++) {
+
+            System.out.printf("%-7d| %-8d| %-7d%n", i, i * i, i*i*i);
 
 
-        for (int i = 1; i <= 100; i += 1) {
-            if (i % 15 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0)
-                System.out.println("Fizz");
-            else if (i % 5 == 0)
-                System.out.println("Buzz");
-            else
-                System.out.println(i);
         }
-
-
     }
 
 
