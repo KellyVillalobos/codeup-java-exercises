9080a268321f01db6f41535dfc3e054f77ad13a2
Kelly Villalobos <kvdesign1978@gmail.com>
2018-05-04 11:04:50 -0500

    adding annotations to Person class

--- src/Person.java
+++ src/Person.java
@@ -3,43 +3,41 @@ import util.Input;
 public class Person {
     public static void main(String[] args) {
 
-//        Person kelly = new Person("Kelly");
-//
-//        System.out.println(kelly);
-//        System.out.println(kelly.getName());
-//        kelly.setName("Kelly Villalobos");
-//        System.out.println(kelly.name);
-//        kelly.sayHello();
-//
-//        Person person1 = new Person("John");
-//        Person person2 = person1;
-//        System.out.println(person1 == person2);
+        Person kelly = new Person("Kelly");
+
+        System.out.println(kelly);
+        System.out.println(kelly.getName());
+        kelly.setName("Kelly Villalobos");
+        System.out.println(kelly.name);
+        kelly.sayHello();
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1 == person2);
 
 
-        Input i = new Input();
-        i.yesNo("Do you want to continue: ");
-    i.getString("Enter a number:");
+
 
     }
 
     private String name;
 
-//---constructor---
+    //---constructor---------
     public Person(String personName) {
             this.name = personName;
     }
-    // returns the person's name
+    //--------returns the person's name-------
 
         public String getName() {
         return name;
     }
 
-    // changes the name property to the passed value
+    //--changes the name property to the passed value--------
     public void setName(String name) {
         this.name = name;
     }
 
-    // prints a message to the console using the person's name
+    //-----prints a message to the console using the person's name-------
     public void sayHello() {
         System.out.println("Hello " + this.name);
     }
